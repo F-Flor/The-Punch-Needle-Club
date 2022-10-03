fbd20cfff232d4ee0f4d3a491e87ea5126150045
Florencia Belen Farias <110876016+F-Flor@users.noreply.github.com>
2022-10-03 18:34:25 -0300

    Corrección de archivos, edición en CSS, agregado de JS.

---
+++ script.js
@@ -0,0 +1,9 @@
+let edad;
+
+edad = prompt("Introduce tu edad")
+
+if (edad >= 18) {
+    alert("Puedes entrar, eres mayor de edad")
+} else {
+    alert("Eres menor de edad, entra bajo tu responsabilidad")
+}
